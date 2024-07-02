c1f48b54382b1cbe1ebc4cce8ea92f990e78cd83
Kuldeep <codysharma01@gmailc.com>
2024-07-02 18:28:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const process = require('process');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fsPromises = require('fs/promises')
+
+const createBrowserless = require('browserless')
+const getHTML = require('html-get')
+
+// Spawn Chromium process once
+const browserlessFactory = createBrowserless()
+
+// Kill the process when Node.js exit
+process.on('exit', () => {
+  console.log('closing resources!')
+  browserlessFactory.close()
+})
+
+function isValidUrl(url) {
+  // Regular expression to check for a valid URL
+  const urlPattern = /^(https:\/\/)?[\w.-]+\.[a-zA-Z]{2,6}(\/\S*)?$/;
+  return urlPattern.test(url);
+}
+
+async function fetchHTML(url) {
+  const browserContext = browserlessFactory.createContext()
+  const getBrowserless = () => browserContext
+  const result = await getHTML(url, { getBrowserless, headers:{
+    'Referer': 'https://www.google.com/',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
+  }, prerender: true  })
+
+  // save the result.html result in a file
+  const tempFilePath = './result.html';
+  await fsPromises.writeFile(tempFilePath, result.html);
+  console.log(`HTML saved to ${tempFilePath}`);
+
+  // close the browser context after it's used
+  await getBrowserless((browser) => browser.destroyContext())
+  return result.html
+}
+
+async function scrape(url) {
+  const lensUrl = `https://lens.google.com/uploadbyurl?url=${encodeURIComponent(url)}`;
+  try {
+    const html = await fetchHTML(lensUrl)
+    const $ = cheerio.load(html);
+
+    const imageUrls = [];
+    $('div.Me0cf img').each((index, element) => {
+      const imgUrl = $(element).attr('src');
+      if (imgUrl && isValidUrl(imgUrl)) {
+        imageUrls.push(imgUrl);
+      }
+    });
+
+    return imageUrls;
+  } catch (error) {
+    console.error(`Error scraping the URL: ${error}`);
+    return null;
+  }
+}
+
+async function processUrl(url) {
+  if (isValidUrl(url)) {
+    return await scrape(url);
+  } else {
+    console.error('Error: Invalid URL');
+    return null;
+  }
+}
+
+// Get the URL from command line arguments
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.error('Error: No URL provided');
+  process.exit(1);
+}
+
+const url = args[0];
+processUrl(url).then((result) => {
+  if (result !== null) {
+    console.log(result);
+  }
+  process.exit(0);
+});
